225443c112523145ffa42406859ad367386736ef
Joe Kramer <joekramer1125@gmail.com>
2017-06-26 10:52:27 -0700

    implement method to make code more DRY

--- MealTime.java
+++ MealTime.java
@@ -2,18 +2,15 @@ import java.io.Console;
 
 public class MealTime {
   public static void main(String[] args) {
+    askWhatYouAteFor("breakfast");
+    askWhatYouAteFor("lunch");
+    askWhatYouAteFor("dinner");
+  }
+
+  public static void askWhatYouAteFor(String meal) {
     Console myConsole = System.console();
-
-    System.out.println("What did you eat for breakfast?");
-    String yourBreakfast = myConsole.readLine();
-    System.out.println("You had " + yourBreakfast + " for breakfast.");
-
-    System.out.println("What did you eat for lunch?");
-    String yourLunch = myConsole.readLine();
-    System.out.println("You had " + yourLunch + " for lunch.");
-
-    System.out.println("What did you eat for dinner?");
-    String yourDinner = myConsole.readLine();
-    System.out.println("You had " + yourDinner + " for dinner.");
+    System.out.println("What did you eat for " + meal + "?");
+    String yourMeal = myConsole.readLine();
+    System.out.println("You had " + yourMeal + " for " + meal + ".");
   }
 }
